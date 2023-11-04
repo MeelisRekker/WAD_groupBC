d8e28a2140712b7ce3ccef837a71096bb21792c2
MeelisRekker <32929450+MeelisRekker@users.noreply.github.com>
2023-11-04 17:23:19 +0200

    Adding posts.js to manage JSON call for posts
    Currently uses JSONBin.io. This remote website modifies entered JSON so it has 'metadata' array and 'record' array. Into last all self written info is stored. There can be many different things there. We only have users array in that array. Look picture for more clarification under 'Helpful' folder.

---
+++ posts.js
@@ -0,0 +1,65 @@
+fetch('https://api.jsonbin.io/v3/b/6545730c54105e766fcb17bc')
+        //fetch('JSON/users.JSON') //Fetch data from local JSON file
+        .then((response) => response.json())
+        .then(json => {
+            let asd = json.record.users.length;
+            console.log(asd);
+            for (let i = 0; i < json.record.users.length; i++) {                
+                let artikkel = document.createElement("article");
+                //artikliPea algus (sisaldab: img ja p)
+                let artikliPea = document.createElement("div");                
+                let avatar = document.createElement("img");
+                if(json.record.users[i].gender === "male"){
+                    avatar.src = 'images/meesAvatar.png';
+                }else{
+                    avatar.src = 'images/meesAvatar.png';
+                }
+                avatar.className = "avatarid";
+                artikliPea.appendChild(avatar);
+                let kuupaev = document.createElement("p");
+                kuupaev.innerText = json.record.users[i].kuupaev;
+                artikliPea.append(kuupaev);
+                artikkel.append(artikliPea);
+                //artikliPea l6pp       
+                //vaatame kas postitusel on pilt juures(kas JSON-is on tühi sõne?)
+                if(json.record.users[i].pilt.length === 0){}
+                else{
+                    let pildiRuum = document.createElement("picture");
+                    let pilt = document.createElement("img");
+                    pilt.src = json.record.users[i].pilt;
+                    pilt.className = "artikkelPilt";
+                    pildiRuum.appendChild(pilt);
+                    let piltText = document.createElement("p");
+                    piltText.className = "pildiFooter";
+                    piltText.innerText = json.record.users[i].piltText; 
+                    pildiRuum.append(piltText);
+                    artikkel.appendChild(pildiRuum);
+                }
+                //postituse sisu
+                let postitus = document.createElement("p");
+                postitus.innerText = json.record.users[i].postitus;                
+                artikkel.appendChild(postitus);
+                //postituse sisu l6pp
+                //thumbs up, thumbs down algus
+                let meeldivus = document.createElement("picture");
+                let like = document.createElement("img");
+                like.src = 'images/thumbsUP.png';
+                like.id = 'meeldib';
+                meeldivus.appendChild(like);
+                artikkel.appendChild(meeldivus);
+                //thumbs up, thumbs down l6pp      
+                document.querySelector(".artikklid").appendChild(artikkel);
+            }
+        })
+    .catch(err => {
+        let errDiv = document.createElement("div");
+        errDiv.className = 'post';
+        errDiv.innerText = err;
+        document.body.appendChild(errDiv);
+    })
+    .finally(() => {
+        let footer = document.createElement("footer");
+        date = new Date().toLocaleString()
+        footer.innerText = date;
+        document.body.appendChild(footer);
+    })
